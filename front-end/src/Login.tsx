ea1d4cd5e8ce440f097007c198063ccd004567fd
José Caique <caique.alves02@gmail.com>
2026-01-27 13:57:03 -0300

    feat: initial login form

---
+++ front-end/src/Login.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Input from "./components/Input";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <form
+      className="flex h-screen items-center justify-center bg-[#161410]"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col items-center justify-center gap-2">
+        <img src="./logo.png" alt="" className="mb-4" />
+        <Input
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="w-full cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm text-white">
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
